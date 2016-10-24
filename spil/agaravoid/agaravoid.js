695ad67c4c60e6a0ec6e10746ec021d4b3bb3d22
Andreas Rudolph <onemanmobile@gmail.com>
2016-10-24 14:36:05 +0200

    agaravoid har nu kun basicspil i sig og mangler implementation der gør det færdigt

--- spil/agaravoid/agaravoid.js
+++ spil/agaravoid/agaravoid.js
@@ -3,12 +3,10 @@ var sketch = function(processingInstance){ with (processingInstance){
 size(400, 400);
 
 // AGAR
-var agarSize = 20;
+var agarSize = 25;
 var agarX = 200;
 var agarY = 200;
 var breakForce = 35;
-var score = 100;
-var highScore = score;
 
 // BOLD
 var ballSize = 40;
@@ -26,15 +24,11 @@ var foodSize = 10;
 var draw = function() {
     background(150, 150, 150);
     
-    //Bevæg Agar mod musen
+    //Bevæg Agar til musen
     
     agarX = mouseX;
     agarY = mouseY;
-    agarSize = score / 5;
     
-    if(score > highScore){
-    highScore = score;
-    }
      
     // tegn agar
     ellipse(agarX,agarY,agarSize,agarSize);
@@ -45,11 +39,7 @@ var draw = function() {
     // tegn bold
     ellipse(ballX, ballY, ballSize, ballSize);
     
-    // tegn liv
-    text("SCORE: " + score, 20, 20);
-    
-    text("HIGHSCORE: " + highScore, width-200, 20);
-    
+  
     
     // find distancen til maden
     // http://www.webmatematik.dk/lektioner/matematik-b/geometri/afstandsformlen
@@ -57,11 +47,11 @@ var draw = function() {
     var yDiff = agarY - foodY;
     var distance = Math.sqrt( xDiff*xDiff + yDiff*yDiff );
      
-    // Hvis distancen fra midten af Agar, til midten af maden er mindre end størrelsen af Agar+Mad, så æder vi maden og gør agar større
+    // Hvis distancen fra midten af Agar, til midten af maden er mindre end størrelsen af Agar+Mad, så æder vi maden og burde gøre agar større
     if(distance < (agarSize / 2) + (foodSize / 2)) {
         foodX = random(50,350);
         foodY = random(50,350);
-        score = score +20;
+       
     }
      
     if (ballX > width || ballX < 0) {
@@ -70,9 +60,7 @@ var draw = function() {
     if (ballY > height || ballY < 0) {
         speedY = - speedY;
     }
-    if( dist(ballX, ballY, agarX, agarY) < (agarSize / 2) + (ballSize / 2)) {
-        score = score - 20;
-    }
+   
     ballX = ballX + speedX;
     ballY = ballY + speedY;
 };
